676a3eec2dad5f3bea15aafa97a3bbf91c195823
jamesossam <JOssam@scu.edu>
2021-03-16 23:20:44 -0400

    Fixed callback uri from spotify auth to match port project runs on

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 function SignIn(props?: { token: string; }) {
-    let spotifyLink = "https://accounts.spotify.com/authorize?client_id=60922fa1b01a4dc097de97dd4030cb1a&redirect_uri=http:%2F%2Flocalhost:8080%2Fcallback&scope=user-read-private%20user-read-email%20user-top-read%20playlist-read-private%20user-read-recently-played%20user-library-read%20playlist-modify-public%20playlist-modify-private&response_type=token";
+    let spotifyLink = "https://accounts.spotify.com/authorize?client_id=60922fa1b01a4dc097de97dd4030cb1a&redirect_uri=http:%2F%2Flocalhost:3000%2Fcallback&scope=user-read-private%20user-read-email%20user-top-read%20playlist-read-private%20user-read-recently-played%20user-library-read%20playlist-modify-public%20playlist-modify-private&response_type=token";
     if (props && props.token) {
         return (
             <div className="signinButton">
