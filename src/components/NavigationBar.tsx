fedbd0bdbaa586120ddf4da06161ca42f3a2793c
jamesossam <JOssam@scu.edu>
2021-04-07 20:24:57 -0700

    Navigation bar now uses router <Link>s instead of <a>s

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,66 +1,66 @@
-import React, { Component, useState } from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
-import { NavLink, Link } from 'react-router-dom';
+/**
+ * Use <Link> tags instead of <a> tags.
+ *
+ * From StackOverflow https://stackoverflow.com/questions/61176344/redux-state-resets-itself-on-route-change-i-think:
+ * Use a <Link> tag as opposed to an <a> tag. The a tag will reload the page entirely and remove current the state. A Link tag allows you to retain your data through out the application but still allows you to move between pages in your app.
+ * */
+
+import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
+
+import '../styles/NavigationBar.css';
 
 interface Props {
     activeScreen: string,
     userSearchedFor: string | null,
 }
 
-function NavigationBar(props: Props) {
-    let meButton = <li className="nav-item"><a className="nav-link" href="/me">Me</a></li>
-    let searchButton = <li className="nav-item"><a className="nav-link" href="/search">Search</a></li>
-    let analysisButton = <li className="nav-item"><a className="nav-link" href="/analysis">Analysis</a></li>
-    //let settingsButton = <li className="nav-item"><a className="nav-link" href="/settings">Settings</a></li>
+function NavigationBar({ activeScreen, userSearchedFor }: Props) {
+  let meButton = <li className="nav-item"><Link to="/me" className="nav-link">Me</Link></li>;
+  let searchButton = <li className="nav-item"><Link to="/search" className="nav-link">Search</Link></li>;
+  let analysisButton = <li className="nav-item"><Link to="/analysis" className="nav-link">Analysis</Link></li>;
 
-    if (props.activeScreen === "me") {
-        meButton = <li className="nav-item active"><a className="nav-link disabled" href="/me">Me</a></li>
-    }
-    else if (props.activeScreen === "search") {
-        searchButton = <li className="nav-item active"><a className="nav-link disabled" href="/search">Search for a user</a></li>
-    }
-    else if (props.activeScreen === "analysis") {
-        analysisButton = <li className="nav-item active"><a className="nav-link disabled" href="/analysis">Analysis</a></li>
-    }
-    /*
-    else if (props.activeScreen === "settings") {
-        settingsButton = <li className="nav-item active"><a className="nav-link disabled" href="/settings">Settings</a></li>
-    }
-     */
+  if (activeScreen === 'me') {
+    meButton = <li className="nav-item active"><Link to="/me" className="nav-link disabledLink" onClick={(event) => event.preventDefault()}>Me</Link></li>;
+  } else if (activeScreen === 'search') {
+    searchButton = <li className="nav-item active"><Link to="/search" className="nav-link disabledLink" onClick={(event) => event.preventDefault()}>Search for a user</Link></li>;
+  } else if (activeScreen === 'analysis') {
+    analysisButton = <li className="nav-item active"><Link to="/analysis" className="nav-link disabledLink" onClick={(event) => event.preventDefault()}>Analysis</Link></li>;
+  }
 
-    // Disable analysis screen if user has not yet searched for another user.
+  // Disable analysis screen if user has not yet searched for another user.
+  if (!userSearchedFor) {
+    analysisButton = <li className="nav-item disabled"><Link to="/analysis" className="nav-link disabledLink" onClick={(event) => event.preventDefault()}>Analysis</Link></li>;
+  }
 
-    console.log(`props.userSearchedFor: ${props.userSearchedFor}`);
-    if (!Boolean(props.userSearchedFor)) {
-        analysisButton = <li className="nav-item disabled"><a className="nav-link disabled" href="/analysis">Analysis</a></li>
-    }
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      {/*
+      <a className="navbar-brand">SharePlay</a>
+      */}
+      <Link className="navbar-brand" to="/" onClick={(event) => event.preventDefault()}>SharePlay</Link>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          {meButton}
 
+          {searchButton}
 
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand">SharePlay</a>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    {meButton}
-
-                    {searchButton}
-
-                    {analysisButton}
-                </ul>
-            </div>
-        </nav>
-    );
+          {analysisButton}
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 const mapStateToProps = (state: any) => ({
-    userSearchedFor: state.userSearchedFor,
-})
+  userSearchedFor: state.userSearchedFor,
+});
 
 NavigationBar.propTypes = {
-    activeScreen: PropTypes.oneOf(['me', 'search', 'analysis', 'settings']).isRequired,
-}
+  activeScreen: PropTypes.oneOf(['me', 'search', 'analysis', 'settings']).isRequired,
+};
 
 export default connect(mapStateToProps)(NavigationBar);
